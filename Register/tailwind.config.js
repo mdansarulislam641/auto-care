151731a9d846bd57cb4149a4156f311cb03d821c
mdansarulislam641 <mdansarulislam641.com>
2023-07-18 21:54:10 +0600

    register and also login page desing done without responsive

---
+++ Register/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors:{
+      primary :  "#69BF70"
+
+    },
+    extend: {
+      fontFamily:{
+        Montserrat : ["Montserrat" , "sens-serif"]
+
+      }
+    },
+  },
+  plugins: [],
+}
+
